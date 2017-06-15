ce0c649da0742735ef04746f219fc4aa52f2dc09
wzc <958364865@qq.com>
2017-06-15 17:11:56 +0800

    个人中心

---
+++ application/home/model/Feedback.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laowang
+ * Date: 2017/3/28
+ * Time: 12:58
+ */
+namespace app\home\model;
+use think\Model;
+class Feedback extends Model{
+    protected $insert = [
+        'create_time' => NOW_TIME
+    ];
+}
