0fd4cffb4952d5a9019ac262c87948cbc6487e1b
rawraw <995106979@qq.com>
2017-06-15 18:58:10 +0800

    意见反馈

---
+++ application/home/model/Years.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/28
+ * Time: 14:37
+ */
+namespace app\home\model;
+
+use think\Model;
+class Years extends Model{
+    protected  $insert = array(
+        'create_time' => NOW_TIME
+    );
+}
