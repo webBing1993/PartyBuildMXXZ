ff1ff532d1871b0801020d3ebf2d84f570a4a079
yangmingzhe <15700004138@163.com>
2017-06-15 10:45:48 +0800

    部门排行 id为2的数据不显示bug修改

--- application/home/controller/Rank.php
+++ application/home/controller/Rank.php
@@ -405,6 +405,7 @@ class Rank extends Base{
                 $new[$key] = $value;
             }
         }
+
         $last = array();
         foreach ($new as $k => $v){
             if($v['id'] == $personal['id']){
@@ -412,11 +413,10 @@ class Rank extends Base{
                 $personal['rank'] = $k+1;
             }
             if($k < 20){ //取小于20名排行
-                if ($v['id'] != 2){
                     $last[$k] = $v;
-                }
             }
         }
+
         $this->assign('all',$last);
         $this ->assign('deparment',$personal);
 
@@ -592,9 +592,7 @@ class Rank extends Base{
         $dpfinals = array();
         foreach ($item as $key =>$value){
             if($key < 20){
-                if ($value['id'] != 2){
                     $dpfinals[] = $value;
-                }
             }
         }
         $this->assign('week',$dpfinals);
@@ -751,9 +749,7 @@ class Rank extends Base{
         $dpfinals_m = array();
         foreach ($item_m as $key =>$value){
             if($key < 20){
-                if ($value['id'] != 2){
-                    $dpfinals_m[] = $value;
-                }
+                $dpfinals_m[] = $value;
             }
         }
         $this->assign('month',$dpfinals_m);
