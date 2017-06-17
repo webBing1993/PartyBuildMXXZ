cbdf7b58510e3a30430ff8a490457ae9aa72f804
wzc <958364865@qq.com>
2017-06-17 17:43:10 +0800

    活动发起

---
+++ application/home/controller/Activity.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 老王
+ * Date: 2017/6/17
+ * Time: 17:40
+ */
+namespace app\home\controller;
+/**
+ *  活动发起  控制器
+ */
+class Activity extends Base{
+    /*
+     * 活动发起 主页 
+     */
+    public function index(){
+        
+    }
+    /*
+     * 投票  发布
+     */
+    public function publish(){
+        
+    }
+}
