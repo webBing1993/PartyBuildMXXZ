4468302c82db7e1e1cacadaf80b5462059e73d3e
yangmingzhe <15700004138@163.com>
2017-06-18 14:54:59 +0800

    活动发布 线上发布完成

--- application/home/controller/Activity.php
+++ application/home/controller/Activity.php
@@ -6,6 +6,7 @@
  * Time: 17:40
  */
 namespace app\home\controller;
+use app\home\model\Wish as  wishModel;
 /**
  *  活动发起  控制器
  */
@@ -20,6 +21,26 @@ class Activity extends Base{
      * 投票  发布
      */
     public function publish(){
-        
+        $data = input('post.');
+        $uid = session('userId');
+        if(empty($data))
+        {
+            return $this ->fetch();
+        }else{
+            $wishModel = new wishModel();
+            $data['type'] = 2;
+            $data['images'] = json_encode($data['images']);
+            $data['publisher'] = get_name($uid);
+            $data['create_time'] = time();
+            $data['create_user'] = $uid;
+            $data['status'] = 0;
+            $wishModel ->data($data) ->save();
+            if($wishModel ->id)
+            {
+                return $this ->success('发布成功!');
+            }else{
+                return $this ->error('发布失败!');
+            }
+        }
     }
 }
---
+++ application/home/model/Wish.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stiff <1570004138@163.com>
+ * Date: 2017/6/18
+ * Time: 14:22
+ */
+namespace app\home\model;
+use think\Model;
+
+class Wish extends Model{
+    
+}
