bc1e0051a739bdf2c60d5e5ad80c6d61ffb69883
stiff1994 <15700004138@163.com>
2017-06-21 18:42:41 +0800

    先锋引领 修改

--- application/home/controller/Pioneer.php
+++ application/home/controller/Pioneer.php
@@ -78,33 +78,54 @@ class Pioneer extends Base {
             return $this ->error('参数错误!');
         }
     }
+
+    /**
+     * 先锋引领首页
+     * @return mixed
+     */
     public function index(){
+        $this ->anonymous();
+        $userId = session('userId');
         $pioneer = new PioneerModel();
-        //党建导师团
-        $list1 = $pioneer ->where(['type' => 1,'status' => ['egt',0]]) ->select();
-        $list1 = $this ->checkLIke($list1);
+        $order = 'likes desc';
         //创业导师团
-        $list2 = $pioneer ->where(['type' => 2,'status' => ['egt',0]]) ->select();
+        $list1 = $pioneer ->where(['type' => 1,'status' => ['egt',0]]) ->order($order) ->select();
+        //创客先锋队
+        $list2 = $pioneer ->where(['type' => 2,'status' => ['egt',0]]) ->order($order) ->select();
         //先进事迹展
         $list3 = $pioneer ->where(['type' => 3,'status' => ['egt',0]]) ->select();
-        
+        //非游客判断是否点赞
+        if($userId != 'visitor'){
+            $list1 = $this ->checkLIke($list1);
+            $list2 = $this ->checkLIke($list2);
+        }
         $this ->assign('list1',$list1);
         $this ->assign('list2',$list2);
         $this ->assign('list3',$list3);
         return $this ->fetch();
     }
 
+    /**
+     * 判断今日导师点赞
+     * @param $data
+     * @return mixed
+     */
     public function checkLIke($data){
         //获取点赞
         $userId = session('userId');
         $likeModel = new Like;
-        foreach($data as $v){
+        foreach($data as $v)
+        {
             $like = $likeModel ->checkLike(5,$v['id'],$userId);
             $v['is_like'] = $like;
         }
         return $data;
     }
 
+    /**
+     * 导师点赞
+     * @return array|void
+     */
     public function like(){
         $data = input('post.');
         $like = new Like();
@@ -119,20 +140,21 @@ class Pioneer extends Base {
             'uid' => $uid
         );
         $res = $like ->where($map) ->find();
+        //今日已点赞
        if(!empty($res))
        {
-            return $this ->error('今日已点赞');
+            return $this ->error('今日已点赞!');
        }else{
            $data['table'] = 'pioneer';
            $data['uid'] = $uid;
            $res = $like ->data($data) ->save();
            if($res) {
                //点赞成功积分+1
-               WechatUser::where('userid',$uid)->setInc('score',1);
-               PioneerModel::where('id',$data['aid'])->setInc('likes',1);
-               return $this->success("点赞成功");
+               WechatUser::where('userid',$uid) ->setInc('score',1);
+               PioneerModel::where('id',$data['aid']) ->setInc('likes',1);
+               return $this ->success("点赞成功");
            }else {
-               return $this->error("点赞失败");
+               return $this->error("点赞失败!");
            }
        }
 
