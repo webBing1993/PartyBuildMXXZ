a7ff53652f355b10b8e73b6ceddc6b8545eee9ef
rawraw <995106979@qq.com>
2017-06-30 20:51:29 +0800

    入党那一天模块

---
+++ application/home/controller/Birth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\home\controller;
+
+/**
+ * Class Birth
+ * @package  入党那一天
+ */
+class Birth extends Base {
+    /**
+     * 主页
+     */
+    public function index(){
+
+        return $this->fetch();
+    }
+
+    /**
+     *  详细页
+     */
+    public function detail(){
+
+        return $this->fetch();
+    }
+}
