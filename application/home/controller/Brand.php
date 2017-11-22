2dcb87d09d18a978ac742f22a98a8934c72e94f2
1324664410@qq.com <hui00544>
2017-11-22 20:36:38 +0800

    服务菜单页面改版

---
+++ application/home/controller/Brand.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/22
+ * Time: 15:58
+ */
+
+namespace app\home\controller;
+
+
+class Brand{
+    /*
+     * 主页
+     */
+    public function index(){
+        return $this->fetch();
+    }
+    /*
+     * 详情页
+     */
+    public function detail(){
+        return $this->fetch();
+    }
+
+}
