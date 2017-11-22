b26590fcda4cb8de2f748485fa900c7b46bc6b6b
ben <750405122@qq.com>
2017-11-22 14:39:25 +0800

    服务菜单模块

--- application/home/controller/Servicemenu.php
+++ application/home/controller/Servicemenu.php
@@ -7,18 +7,30 @@
  */
 namespace app\home\controller;
 use app\home\model\WechatUser;
+use app\home\model\ServiceItems;
 
 class Servicemenu extends Base{
     /*
      * 主页
      */
     public function index(){
+        $modelAll = [];
+        $result = ServiceItems::where('1=1')->group('department_id')->order('department_id')->column('department_id');
+        foreach ($result as $key => $department_id){
+            $modelAll[$key]['name'] = ServiceItems::where(['department_id' => $department_id])->value('department');
+            $modelAll[$key]['data'] = ServiceItems::where(['department_id' => $department_id])->order('id')->column('id,name');
+        }
+//        var_dump($modelAll);die;
+        $this->assign('modelAll',$modelAll);
         return $this->fetch();
     }
     /*
      * 组织架构详情页
      */
     public function detail(){
+        $pid = input('id');
+        $model = ServiceItems::where(['id' => $pid])->find();
+        $this->assign('model',$model);
         return $this->fetch();
     }
 }
---
+++ application/home/model/ServiceItems.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 2017/11/22
+ * Time: 下午1:29
+ */
+
+namespace app\home\model;
+use think\Model;
+
+class ServiceItems extends Model
+{
+
+}
