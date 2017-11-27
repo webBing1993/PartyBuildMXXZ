62a2bc38be290310a730f2cc8f1ae3675074ac97
ben <750405122@qq.com>
2017-11-27 17:22:49 +0800

    网上述职

--- application/home/controller/Report.php
+++ application/home/controller/Report.php
@@ -38,11 +38,76 @@ class Report extends Base{
         $this->assign('list',$list);
         return $this ->fetch();
     }
+    /**
+     * 主页加载更多
+     */
+    public function indexmore(){
+        $len = input('length');
+        $map = array(
+            'status' => array('egt',0),
+        );
+        $list = ReportModel::where($map)->order('id desc')->limit($len,5)->select();
+        foreach($list as $value){
+            $value['year'] = date('Y', $value['create_time']);
+            $value['month'] = date('m', $value['create_time']);
+            $value['day'] = date('d', $value['create_time']);
+        }
+        if($list){
+            return $this->success("加载成功",'',$list);
+        }else{
+            return $this->error("加载失败");
+        }
+    }
 
     /**
      * 详情页
      */
     public function detail(){
+        $this->anonymous();        //判断是否是游客
+        $this->jssdk();
+
+        $userId = session('userId');
+        $id = input('id');
+        $learnModel = new ReportModel();
+        //浏览加一
+        $info['views'] = array('exp','`views`+1');
+        $learnModel::where('id',$id)->update($info);
+        if($userId != "visitor"){
+            //浏览不存在则存入pb_browse表
+            $con = array(
+                'user_id' => $userId,
+                'report_id' => $id,
+            );
+            $history = Browse::get($con);
+            if(!$history && $id != 0){
+                $s['score'] = array('exp','`score`+1');
+                if ($this->score_up()){
+                    // 未满 15 分
+                    Browse::create($con);
+                    WechatUser::where('userid',$userId)->update($s);
+                }
+            }
+        }
+        $article = $learnModel::get($id);
+        $article['user'] = session('userId');
+        //分享图片及链接及描述
+        $image = Picture::where('id',$article['front_cover'])->find();
+        $article['share_image'] = "http://".$_SERVER['SERVER_NAME'].$image['path'];
+        $article['link'] = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REDIRECT_URL'];
+        $article['desc'] = str_replace('&nbsp;','',strip_tags($article['content']));
+        $article['desc'] = str_replace(" ",'',$article['desc']);
+        $article['desc'] = str_replace("\n",'',$article['desc']);
+
+        //获取 文章点赞
+        $likeModel = new Like;
+        $like = $likeModel->getLike(13,$id,$userId);
+        $article['is_like'] = $like;
+        $this->assign('article',$article);
+
+        //获取 评论
+        $commentModel = new Comment();
+        $comment = $commentModel->getComment(13,$id,$userId);
+        $this->assign('comment',$comment);
         return $this->fetch();
     }
 }
