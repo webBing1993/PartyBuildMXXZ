ff054b4f9d3f3d6f673bf0f800aff3a7dd54bc02
ben <750405122@qq.com>
2017-11-30 13:46:14 +0800

    品牌项目详情页加接口

--- application/home/controller/Brand.php
+++ application/home/controller/Brand.php
@@ -8,6 +8,7 @@
 
 namespace app\home\controller;
 use app\home\model\WechatUser;
+use think\Collection;
 use think\Controller;
 use app\home\model\Comment;
 use app\admin\model\Picture;
@@ -50,6 +51,27 @@ class Brand extends Base{
         }
         $this->assign('list',$list);
         return $this->fetch();
+    }
+    /*
+     * 详情页接口
+     */
+    public function branddetailapi(){
+        $id = input('id');
+        $map = array(
+            'status' => ['egt',0],
+            'pid' => $id,
+        );
+        $list = BrandDetailModel::where($map)->order('id desc')->select();
+        foreach ($list as $model){
+            $model['images'] = json_decode($model['images'], true);
+        }
+        if($list){
+//            $collection = new Collection($list);
+//            $list = $collection->toArray();
+            return $this->success("加载成功", '', $list);
+        }else{
+            return $this->error("加载失败");
+        }
     }
 
     public function detail(){
